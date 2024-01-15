13a4e265e4439a2a778690b89bcba62b08bd124b
Racostadev <[200~racostawebdev@gmail.com>
2024-01-15 16:17:54 +0100

    adjust Terms

--- src/ComponentsShoppingCar/ShoppingCar.jsx
+++ src/ComponentsShoppingCar/ShoppingCar.jsx
@@ -1,17 +1,19 @@
-
+import { FiShoppingCart } from "react-icons/fi";
 
 const ShoppingCar = () => {
   return (
-    <div>
-     
-
-<h1> 
-
-  carrooooooooooooooooooo
-</h1>
-
-
-    </div>
+    <section className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md text-center">
+        <h1 className="text-2xl font-semibold mb-4">
+          <FiShoppingCart className="inline-block mr-2" />
+          Tu carrito está vacío
+        </h1>
+        <p className="text-gray-600 mb-8">Empieza a comprar para añadir productos a tu carrito.</p>
+        <button className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+          Empieza a comprar
+        </button>
+      </div>
+    </section>
   );
 };
 
