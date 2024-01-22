f833a15fe963e9c7966c5045ae9867bda1aa28dd
Gustavo <148284090+gustavoflorez20@users.noreply.github.com>
2024-01-22 19:29:16 +0100

    producto

--- src/ComponentsFrozen/Frozen.jsx
+++ src/ComponentsFrozen/Frozen.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AmountContext } from "../ComponentsContex/Cant";
+
+const Displayamount = () => {
+  const { amount } = useContext(AmountContext);
+  return <p>{amount}</p>;
+};
+
 
 
 const Frozen = () => {
+  const { addition, subtract } = useContext(AmountContext);
+ 
+
+  const handlePruebaloClick = () => {
+    addition(); 
+    subtract(); 
+  
+
+  }
   return (
     <div className="bg-gray-100 min-h-screen py-12 flex items-center justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -23,9 +39,29 @@ const Frozen = () => {
           <ul className="text-sm text-gray-600 mb-6"></ul>
         </div>
         <div className="p-4">
-          <button className="w-full bg-orange-500 text-white rounded-full px-4 py-2 hover:bg-orange-700 focus:outline-none focus:shadow-outline-purple active:bg-orange-800">
-            Pruebalo!
-          </button>
+        <button
+  className="w-full bg-orange-500 text-white rounded-full px-4 py-2 hover:bg-orange-700 focus:outline-none focus:shadow-outline-purple active:bg-orange-800"
+  onClick={(e) => {
+    if (e.target.innerText === '+') {
+      addition();
+    } else if (e.target.innerText === 'Pruebalo') {
+      addition();
+      console.log("Hacer algo específico al hacer clic en 'Pruebalo'");
+    } else if (e.target.innerText === '-') {
+      
+      subtract();
+    }
+  }}
+>
+  <button style={{ marginRight: '80px' }}>+</button>
+  <button>Pruebalo</button>
+  <button style={{ marginLeft: '80px' }}>-</button>
+  <Displayamount /> 
+</button>
+            
+           
+
+          
         </div>
       </div>
       {/* <!-- Pricing Card 2 --> */}
@@ -62,29 +98,6 @@ const Frozen = () => {
             className="mb-6 w-full h-auto" 
             src="https://scontent.fmad22-1.fna.fbcdn.net/v/t39.30808-6/245256122_1806969332824561_3594248169345443282_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=c83dfd&_nc_ohc=95066_CTJj4AX-m3FWn&_nc_ht=scontent.fmad22-1.fna&oh=00_AfCmHqP8tb7QcGsqgb3Qit8EEkGb98SQRNo8ukCtrF5X4g&oe=6595FF94" 
             alt="Tequeyoyo"
-          />
-          <p className="text-4xl font-bold text-gray-800 mb-6"></p>
-          <ul className="text-sm text-gray-600 mb-6"></ul>
-        </div>
-        <div className="p-4">
-          <button className="w-full bg-orange-500 text-white rounded-full px-4 py-2 hover:bg-orange-700 focus:outline-none focus:shadow-outline-purple active:bg-orange-800">
-            Pruebalo!
-          </button>
-        </div>
-      </div>
-
-      {/* <!-- Pricing Card 4 --> */}
-      <div className="bg-white rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
-        <div className="p-1 bg-orange-200"></div>
-        <div className="p-8">
-          <h2 className="text-3xl font-bold text-gray-800 mb-5">
-            Cachito Jamon 
-          </h2>
-
-          <img
-            className="mb-6 w-full h-auto" 
-            src="https://scontent.fmad22-1.fna.fbcdn.net/v/t1.6435-9/164059961_1652867384901424_8254027222998946438_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=810d5f&_nc_ohc=NHTS-G2rAT0AX8Iag7Q&_nc_ht=scontent.fmad22-1.fna&oh=00_AfBq1HsMrCNVTTtC9xVdyHKpTIa_rhjttCIitFMc9EAzKw&oe=65BE860F" 
-            alt="Cachito Jamon"
           />
           <p className="text-4xl font-bold text-gray-800 mb-6"></p>
           <ul className="text-sm text-gray-600 mb-6"></ul>
